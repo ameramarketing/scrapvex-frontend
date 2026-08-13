bb2d917b9107fe49938514e662692da3d58065f7
scrapvex-admin <amera.digitalmarketing@gmail.com>
2026-08-13 07:14:17 +0530

    fix(admin): add missing FaMoon and FaSun icon imports in AdminDashboard.jsx to resolve white screen crash

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -5,7 +5,7 @@ import {
   FaSignOutAlt, FaTrash, FaPlus, FaKey, FaBell, FaInfoCircle,
   FaAd, FaTag, FaTools, FaStar, FaUserPlus, FaBars, FaTimes, FaCog,
   FaEnvelope, FaPhone, FaMapMarkerAlt, FaFacebook, FaInstagram, FaRecycle, FaWallet, FaHistory, FaArrowUp, FaArrowDown, FaChartLine,
-  FaFileInvoice, FaBuilding, FaIdCard, FaCar, FaUserCheck, FaMap, FaTicketAlt, FaPercent, FaShareAlt, FaRss, FaClipboardList, FaMoneyCheckAlt, FaBoxOpen, FaWhatsapp
+  FaFileInvoice, FaBuilding, FaIdCard, FaCar, FaUserCheck, FaMap, FaTicketAlt, FaPercent, FaShareAlt, FaRss, FaClipboardList, FaMoneyCheckAlt, FaBoxOpen, FaWhatsapp, FaMoon, FaSun
 } from "react-icons/fa";
 import API from "../services/api";
 import Toast from "../components/Toast";
